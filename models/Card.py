5043504caacdedc5c2f53d6d1cb4d484c04edbd7
unknown <songyanghan+git@gmail.com>
2023-03-12 13:43:01 +0800

    EDIT: modified Card class to use dataclass, add comparison dunder methods

--- models/Card.py
+++ models/Card.py
@@ -1,19 +1,30 @@
 from models.Rank import Rank
 from models.Suit import Suit
 from models.Constants import CARD_RANKS, CARD_SUITS
+from dataclasses import dataclass
 
+@dataclass(frozen=True, slots=True)
 class Card:
-    def __init__(self, rank: str, suit: str) -> None:
-        if rank not in CARD_RANKS:
-            raise ValueError(f'Rank invalid, got {rank}')
-        if suit not in CARD_SUITS:
-            raise ValueError(f'Suit invalid, got {suit}')
-        self.rank = rank
-        self.suit = suit
+    rank: str
+    suit: str
 
+    def __post_init__(self) -> None:
+        if self.rank not in CARD_RANKS:
+            raise ValueError(f'Rank invalid, got {self.rank}')
+        if self.suit not in CARD_SUITS:
+            raise ValueError(f'Suit invalid, got {self.suit}')
 
     def __str__(self) -> str:
         return f'{Rank(self.rank).name} OF {Suit(self.suit).name}S'
 
     def __repr__(self) -> str:
         return f'{self.rank}{self.suit}'
+    
+    def __members(self) -> tuple[str]:
+        return (self.rank, self.suit)
+
+    def __eq__(self, other) -> bool:
+        return self.__members() == other.__members()
+    
+    def __hash__(self) -> int:
+        return hash(self.__members())
--- tests/test_card.py
+++ tests/test_card.py
@@ -6,3 +6,12 @@ def test_card_raises_error_when_initialized_with_invalid_parameters():
         Card('invalid_value','h')
     with pytest.raises(ValueError):
         Card('2','invalid_value')
+
+def test_equals_returns_true_when_equal():
+    assert Card('5', 'h') == Card('5', 'h')
+    assert Card('t', 'c') == Card('t', 'c')
+
+def test_equal_returns_false_when_not_equal():
+    assert Card('5', 'h') != Card('6', 'h')
+    assert Card('5', 'h') != Card('5', 'c')
+    assert Card('5', 'h') != Card('t', 'd')
