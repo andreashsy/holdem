393f4aeea7bd8a24f8f0c68a8b4ad654cce0fb73
unknown <songyanghan+git@gmail.com>
2023-04-17 22:57:50 +0800

    DEL: remove Constants

--- models/Constants.py
+++
@@ -1,7 +0,0 @@
-from models.Rank import Rank
-from models.Suit import Suit
-from models.HandRank import HandRank
-
-CARD_SUITS = [s.value for s in Suit]
-CARD_RANKS = [r.value for r in Rank]
-HANDRANKS = [rank.name for rank in HandRank]
