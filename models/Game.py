22ba8efc8290d611e05a1f7b8b1f08f6daf2ddf8
unknown <songyanghan+git@gmail.com>
2023-05-01 22:26:22 +0800

    add game and modify player

---
+++ models/Game.py
@@ -0,0 +1,29 @@
+from models.Deck import Deck
+from models.Player import Player
+
+class HoldemGameState:
+    # def __init__(self, players: list[Player], deck: Deck, big_blind: int = 2) -> None:
+    #     self.players = players
+    #     self.deck = deck
+    #     self.pot = 0
+    #     self.community_cards = []
+    #     self.big_blind = big_blind
+    pass
+
+"""
+when all player have joined, init game
+shuffle players into random order
+post blinds
+deal hole cards
+wait for all active bets to be equal
+deal flop
+wait for all active bets to be equal
+deal turn
+wait for all active bets to be equal
+deal river
+wait for all active bets to be equal
+showdown: reveal last aggresor and winning hand
+pot goes to winner
+shift player order by 1
+jump to post blinds step
+"""
--- models/Player.py
+++ models/Player.py
@@ -1,9 +1,14 @@
-class Player:
-    def __init__(self) -> None:
-        pass
+from models.Card import Card
 
+
+class Player:
+    # def __init__(self, stack: int=0) -> None:
+    #     self.stack = stack
+    #     self.stack_committed = 0
+    #     self.current_bet = 0
+    #     self.is_in_game = False
+    #     self.hole_cards = []
     # 2 cards, stack, stack_committed, has_folded, current_bet
 
     # table - 5 cards, player order, player turn, pot size, blinds, min_raise
-
-    # logic - determine best hand from 5/6/7 cards (separate class)
+    pass
