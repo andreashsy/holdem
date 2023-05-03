e5a87e4d64efda14cf265344abc6a0bc45305865
unknown <songyanghan+git@gmail.com>
2023-05-03 23:14:59 +0800

    flesh out game structure

--- models/Game.py
+++ models/Game.py
@@ -1,16 +1,87 @@
+from models.Card import Card
 from models.Deck import Deck
 from models.Player import HoldemPlayer
+from models.GamePhase import HoldemGamePhase
+import random
 
 class HoldemGameState:
-    def __init__(self, players: list[HoldemPlayer]) -> None:
+    def __init__(self, players: list[HoldemPlayer], big_blind: int=2, small_blind: int=1) -> None:
         if len(players) < 2: raise ValueError("Game requires at least 2 players")
-    # def __init__(self, players: list[Player], deck: Deck, big_blind: int = 2) -> None:
-    #     self.players = players
-    #     self.deck = deck
-    #     self.pot = 0
-    #     self.community_cards = []
-    #     self.big_blind = big_blind
+        self.players = players
+        self.phase = HoldemGamePhase.PREGAME
+        self.deck = None
+        self.big_blind = big_blind
+        self.small_blind = small_blind
+        self.community_cards = []
+        # self.active_player_index = None
 
+    def start_preflop(self) -> None:
+        if not self.are_all_bets(0): raise ValueError('All bets must be zero')
+        if self.phase == HoldemGamePhase.PREGAME:
+            self.shuffle_players()
+        elif self.phase == HoldemGamePhase.SHOWDOWN:
+            self.advance_button()
+        else: 
+            raise ValueError(f"Game phase must be pregame or showdown. Current mode is {self.phase}")
+        self.phase = HoldemGamePhase.PREFLOP
+        self.prepare_deck()
+        self.pay_blinds()
+        self.deal_hole_cards()
+    
+    def start_flop(self) -> None:
+        if not self.phase == HoldemGamePhase.PREFLOP: raise ValueError(f"Game must be in preflop. Current mode is {self.phase}")
+        self.phase = HoldemGamePhase.FLOP
+        card1, card2, card3 = self.deck.draw_card(), self.deck.draw_card(), self.deck.draw_card()
+        self.community_cards.extend([card1, card2, card3])
+
+    def start_turn(self) -> None:
+        if not self.phase == HoldemGamePhase.FLOP: raise ValueError(f"Game must be in flop. Current mode is {self.phase}")
+        self.phase = HoldemGamePhase.TURN
+        self.community_cards.append(self.deck.draw_card())
+
+    def start_river(self) -> None:
+        if not self.phase == HoldemGamePhase.TURN: raise ValueError(f"Game must be in turn. Current mode is {self.phase}")
+        self.phase = HoldemGamePhase.RIVER
+        self.community_cards.append(self.deck.draw_card())
+
+    def start_showdown(self) -> None:
+        if not self.phase == HoldemGamePhase.RIVER: raise ValueError(f"Game must be in river. Current mode is {self.phase}")
+        self.phase = HoldemGamePhase.SHOWDOWN
+
+    def initiate_betting_round(self) -> None:
+        pass
+
+
+    def advance_button(self) -> None:
+        self.players = self.players[1:] + [self.players[0]]
+
+    def prepare_deck(self) -> None:
+        self.deck = Deck()
+        self.deck.shuffle()
+
+    def shuffle_players(self) -> None:
+        random.shuffle(self.players)
+
+    def pay_blinds(self) -> None:
+        self.players[-1].bet(self.big_blind)
+        self.players[-2].bet(self.small_blind)
+
+    def deal_hole_cards(self) -> None:
+        for player in self.players:
+            card1, card2 = self.deck.draw_card(), self.deck.draw_card()
+            player.hole_cards.extend([card1, card2])
+
+    def are_all_players_active(self) -> bool:
+        return all([player.is_active for player in self.players])
+    
+    def are_all_bets(self, bet_value: int) -> bool:
+        return all([player.get_current_bet() == bet_value for player in self.players])
+    
+    def are_all_active_bets(self, bet_value: int) -> bool:
+        return all([player.get_current_bet() == bet_value for player in self.players if player.is_active])
+
+    def get_community_cards(self) -> list[Card]:
+        return self.community_cards
 """
 when all player have joined, init game
 shuffle players into random order
--- tests/test_game.py
+++ tests/test_game.py
@@ -2,10 +2,472 @@ import pytest
 
 from models.Game import HoldemGameState
 from models.Player import HoldemPlayer
+from models.GamePhase import HoldemGamePhase
 
 def test_holdemgamestate_raises_if_initialised_with_less_than_2_players():
     with pytest.raises(ValueError):
         HoldemGameState(players=[])
-    # with pytest.raises(ValueError):
-    #     HoldemGameState(players=[HoldemPlayer()])
+    with pytest.raises(ValueError):
+        HoldemGameState(players=[HoldemPlayer(stack=5)])
 
+def test_holdemgamestate_initialises_at_pregame_phase():
+    players = [HoldemPlayer(stack=5, id="p1"), HoldemPlayer(stack=7, id="p2")]
+    game = HoldemGameState(players=players)
+
+    assert game.phase == HoldemGamePhase.PREGAME
+
+def test_holdemgamestate_has_same_players_as_initialised():
+    players = [HoldemPlayer(stack=5, id="p1"), HoldemPlayer(stack=7, id="p2")]
+    game = HoldemGameState(players=players)
+
+    assert game.players == players
+
+def test_are_all_players_active_returns_true_if_all_players_active():
+    p1 = HoldemPlayer(stack=5, id="p1")
+    p2 = HoldemPlayer(stack=7, id="p2")
+    p1.participate()
+    p2.participate()
+    players = [p1, p2]
+    game = HoldemGameState(players=players)
+
+    assert game.are_all_players_active() == True
+
+def test_are_all_players_active_returns_false_if_one_player_not_active():
+    p1 = HoldemPlayer(stack=5, id="p1")
+    p2 = HoldemPlayer(stack=7, id="p2")
+    p2.participate()
+    players = [p1, p2]
+    game = HoldemGameState(players=players)
+
+    assert game.are_all_players_active() == False
+
+def test_are_all_players_active_returns_false_if_all_players_not_active():
+    p1 = HoldemPlayer(stack=5, id="p1")
+    p2 = HoldemPlayer(stack=7, id="p2")
+    players = [p1, p2]
+    game = HoldemGameState(players=players)
+
+    assert game.are_all_players_active() == False
+
+def test_start_preflop_changes_phase_to_preflop_if_phase_in_pregame():
+    p1 = HoldemPlayer(stack=5, id="p1")
+    p2 = HoldemPlayer(stack=7, id="p2")
+    p1.participate()
+    p2.participate()
+    players = [p1, p2]
+    game = HoldemGameState(players=players)
+    game.start_preflop()
+
+    assert game.phase == HoldemGamePhase.PREFLOP
+
+def test_start_preflop_changes_phase_to_preflop_if_phase_in_showdown():
+    p1 = HoldemPlayer(stack=5, id="p1")
+    p2 = HoldemPlayer(stack=7, id="p2")
+    p1.participate()
+    p2.participate()
+    players = [p1, p2]
+    game = HoldemGameState(players=players)
+    game.phase = HoldemGamePhase.SHOWDOWN
+    game.start_preflop()
+
+    assert game.phase == HoldemGamePhase.PREFLOP
+
+def test_start_preflop_raises_if_not_all_bets_zero():
+    p1 = HoldemPlayer(stack=5, id="p1")
+    p2 = HoldemPlayer(stack=7, id="p2")
+    p1.participate()
+    p2.participate()
+    p2.bet(1)
+    players = [p1, p2]
+    game = HoldemGameState(players=players)
+    with pytest.raises(ValueError):
+        game.start_preflop()
+
+def test_start_preflop_raises_if_game_not_in_pregame_or_showdown_phase():
+    players = [HoldemPlayer(stack=5, id="p1"), HoldemPlayer(stack=7, id="p2")]
+    game = HoldemGameState(players=players)
+    
+    with pytest.raises(ValueError):
+        game.phase = HoldemGamePhase.PREFLOP
+        game.start_preflop()
+    with pytest.raises(ValueError):
+        game.phase = HoldemGamePhase.FLOP
+        game.start_preflop()
+    with pytest.raises(ValueError):
+        game.phase = HoldemGamePhase.TURN
+        game.start_preflop()
+    with pytest.raises(ValueError):
+        game.phase = HoldemGamePhase.RIVER
+        game.start_preflop()
+
+def test_start_flop_changes_phase_to_flop():
+    p1 = HoldemPlayer(stack=5, id="p1")
+    p2 = HoldemPlayer(stack=7, id="p2")
+    p1.participate()
+    p2.participate()
+    players = [p1, p2]
+    game = HoldemGameState(players=players)
+    game.start_preflop()
+    game.start_flop()
+
+    assert game.phase == HoldemGamePhase.FLOP
+
+def test_start_flop_raises_if_game_not_in_preflop():
+    players = [HoldemPlayer(stack=5, id="p1"), HoldemPlayer(stack=7, id="p2")]
+    game = HoldemGameState(players=players)
+    
+    with pytest.raises(ValueError):
+        game.phase = HoldemGamePhase.PREGAME
+        game.start_flop()
+    with pytest.raises(ValueError):
+        game.phase = HoldemGamePhase.FLOP
+        game.start_flop()
+    with pytest.raises(ValueError):
+        game.phase = HoldemGamePhase.TURN
+        game.start_flop()
+    with pytest.raises(ValueError):
+        game.phase = HoldemGamePhase.RIVER
+        game.start_flop()
+    with pytest.raises(ValueError):
+        game.phase = HoldemGamePhase.SHOWDOWN
+        game.start_flop()
+
+def test_start_flop_deals_3_cards_to_community_cards():
+    p1 = HoldemPlayer(stack=5, id="p1")
+    p2 = HoldemPlayer(stack=7, id="p2")
+    p1.participate()
+    p2.participate()
+    players = [p1, p2]
+    game = HoldemGameState(players=players)
+    game.start_preflop()
+    game.start_flop()
+
+    assert len(game.get_community_cards()) == 3
+
+def test_start_turn_changes_phase_to_turn():
+    p1 = HoldemPlayer(stack=5, id="p1")
+    p2 = HoldemPlayer(stack=7, id="p2")
+    p1.participate()
+    p2.participate()
+    players = [p1, p2]
+    game = HoldemGameState(players=players)
+    game.start_preflop()
+    game.start_flop()
+    game.start_turn()
+
+    assert game.phase == HoldemGamePhase.TURN
+
+def test_start_turn_raises_if_game_not_in_flop():
+    players = [HoldemPlayer(stack=5, id="p1"), HoldemPlayer(stack=7, id="p2")]
+    game = HoldemGameState(players=players)
+    
+    with pytest.raises(ValueError):
+        game.phase = HoldemGamePhase.PREGAME
+        game.start_turn()
+    with pytest.raises(ValueError):
+        game.phase = HoldemGamePhase.PREFLOP
+        game.start_turn()
+    with pytest.raises(ValueError):
+        game.phase = HoldemGamePhase.TURN
+        game.start_turn()
+    with pytest.raises(ValueError):
+        game.phase = HoldemGamePhase.RIVER
+        game.start_turn()
+    with pytest.raises(ValueError):
+        game.phase = HoldemGamePhase.SHOWDOWN
+        game.start_turn()
+
+def test_start_turn_causes_community_cards_to_have_4_cards():
+    p1 = HoldemPlayer(stack=5, id="p1")
+    p2 = HoldemPlayer(stack=7, id="p2")
+    p1.participate()
+    p2.participate()
+    players = [p1, p2]
+    game = HoldemGameState(players=players)
+    game.start_preflop()
+    game.start_flop()
+    game.start_turn()
+
+    assert len(game.get_community_cards()) == 4
+
+def test_start_river_changes_phase_to_river():
+    p1 = HoldemPlayer(stack=5, id="p1")
+    p2 = HoldemPlayer(stack=7, id="p2")
+    p1.participate()
+    p2.participate()
+    players = [p1, p2]
+    game = HoldemGameState(players=players)
+    game.start_preflop()
+    game.start_flop()
+    game.start_turn()
+    game.start_river()
+
+    assert game.phase == HoldemGamePhase.RIVER
+
+def test_start_river_raises_if_game_not_in_turn():
+    players = [HoldemPlayer(stack=5, id="p1"), HoldemPlayer(stack=7, id="p2")]
+    game = HoldemGameState(players=players)
+    
+    with pytest.raises(ValueError):
+        game.phase = HoldemGamePhase.PREGAME
+        game.start_river()
+    with pytest.raises(ValueError):
+        game.phase = HoldemGamePhase.PREFLOP
+        game.start_river()
+    with pytest.raises(ValueError):
+        game.phase = HoldemGamePhase.FLOP
+        game.start_river()
+    with pytest.raises(ValueError):
+        game.phase = HoldemGamePhase.RIVER
+        game.start_river()
+    with pytest.raises(ValueError):
+        game.phase = HoldemGamePhase.SHOWDOWN
+        game.start_river()
+
+def test_start_showdown_changes_phase_to_river():
+    p1 = HoldemPlayer(stack=5, id="p1")
+    p2 = HoldemPlayer(stack=7, id="p2")
+    p1.participate()
+    p2.participate()
+    players = [p1, p2]
+    game = HoldemGameState(players=players)
+    game.start_preflop()
+    game.start_flop()
+    game.start_turn()
+    game.start_river()
+    game.start_showdown()
+
+    assert game.phase == HoldemGamePhase.SHOWDOWN
+
+def test_start_showdown_raises_if_game_not_in_river():
+    players = [HoldemPlayer(stack=5, id="p1"), HoldemPlayer(stack=7, id="p2")]
+    game = HoldemGameState(players=players)
+    
+    with pytest.raises(ValueError):
+        game.phase = HoldemGamePhase.PREGAME
+        game.start_showdown()
+    with pytest.raises(ValueError):
+        game.phase = HoldemGamePhase.PREFLOP
+        game.start_showdown()
+    with pytest.raises(ValueError):
+        game.phase = HoldemGamePhase.FLOP
+        game.start_showdown()
+    with pytest.raises(ValueError):
+        game.phase = HoldemGamePhase.TURN
+        game.start_showdown()
+    with pytest.raises(ValueError):
+        game.phase = HoldemGamePhase.SHOWDOWN
+        game.start_showdown()
+
+def test_start_river_causes_community_cards_to_have_5_cards():
+    p1 = HoldemPlayer(stack=5, id="p1")
+    p2 = HoldemPlayer(stack=7, id="p2")
+    p1.participate()
+    p2.participate()
+    players = [p1, p2]
+    game = HoldemGameState(players=players)
+    game.start_preflop()
+    game.start_flop()
+    game.start_turn()
+    game.start_river()
+
+    assert len(game.get_community_cards()) == 5
+
+def test_advance_button_shifts_2_player_order_correctly():
+    p1 = HoldemPlayer(stack=5, id="p1")
+    p2 = HoldemPlayer(stack=7, id="p2")
+    p1.participate()
+    p2.participate()
+    players = [p1, p2]
+    game = HoldemGameState(players=players)
+
+    game.advance_button()
+
+    assert game.players == [p2, p1]
+
+def test_advance_button_shifts_5_player_order_correctly():
+    p1 = HoldemPlayer(stack=5, id="p1")
+    p2 = HoldemPlayer(stack=7, id="p2")
+    p3 = HoldemPlayer(stack=7, id="p3")
+    p4 = HoldemPlayer(stack=7, id="p4")
+    p5 = HoldemPlayer(stack=7, id="p5")
+    p1.participate()
+    p2.participate()
+    p3.participate()
+    p4.participate()
+    p5.participate()
+    players = [p1, p2, p3, p4, p5]
+    game = HoldemGameState(players=players)
+
+    game.advance_button()
+
+    assert game.players == [p2, p3, p4, p5, p1]
+
+def test_pay_blinds_deduct_default_blinds_correctly():
+    p1 = HoldemPlayer(stack=5, id="p1")
+    p2 = HoldemPlayer(stack=7, id="p2")
+    p3 = HoldemPlayer(stack=7, id="p3")
+    p4 = HoldemPlayer(stack=7, id="p4")
+    p5 = HoldemPlayer(stack=7, id="p5")
+    p1.participate()
+    p2.participate()
+    p3.participate()
+    p4.participate()
+    p5.participate()
+    players = [p1, p2, p3, p4, p5]
+    game = HoldemGameState(players=players)
+
+    game.pay_blinds()
+
+    p4_bet = p4.get_current_bet()
+    p5_bet = p5.get_current_bet()
+    assert p5_bet == 2
+    assert p4_bet == 1
+
+def test_pay_blinds_deduct_custom_blinds_correctly():
+    p1 = HoldemPlayer(stack=50, id="p1")
+    p2 = HoldemPlayer(stack=70, id="p2")
+    p3 = HoldemPlayer(stack=70, id="p3")
+    p4 = HoldemPlayer(stack=70, id="p4")
+    p5 = HoldemPlayer(stack=70, id="p5")
+    p1.participate()
+    p2.participate()
+    p3.participate()
+    p4.participate()
+    p5.participate()
+    players = [p1, p2, p3, p4, p5]
+    big_blind = 10
+    small_blind = 4
+    game = HoldemGameState(players=players, big_blind=big_blind, small_blind=small_blind)
+
+    game.pay_blinds()
+
+    p4_bet = p4.get_current_bet()
+    p5_bet = p5.get_current_bet()
+    assert p5_bet == big_blind
+    assert p4_bet == small_blind
+
+def test_deal_hole_cards_gives_2_players_2_cards_each():
+    p1 = HoldemPlayer(stack=5, id="p1")
+    p2 = HoldemPlayer(stack=7, id="p2")
+    p1.participate()
+    p2.participate()
+    players = [p1, p2]
+    game = HoldemGameState(players=players)
+
+    game.prepare_deck()
+    game.deal_hole_cards()
+
+    assert len(p1.hole_cards) == 2
+    assert len(p2.hole_cards) == 2
+
+def test_deal_hole_cards_gives_5_players_2_cards_each():
+    p1 = HoldemPlayer(stack=5, id="p1")
+    p2 = HoldemPlayer(stack=7, id="p2")
+    p3 = HoldemPlayer(stack=7, id="p3")
+    p4 = HoldemPlayer(stack=7, id="p4")
+    p5 = HoldemPlayer(stack=7, id="p5")
+    p1.participate()
+    p2.participate()
+    p3.participate()
+    p4.participate()
+    p5.participate()
+    players = [p1, p2, p3, p4, p5]
+    game = HoldemGameState(players=players)
+
+    game.prepare_deck()
+    game.deal_hole_cards()
+
+    assert len(p1.hole_cards) == 2
+    assert len(p2.hole_cards) == 2
+    assert len(p3.hole_cards) == 2
+    assert len(p4.hole_cards) == 2
+    assert len(p5.hole_cards) == 2
+
+def test_are_all_bets_zero_returns_true_if_all_zero():
+    p1 = HoldemPlayer(stack=5, id="p1")
+    p2 = HoldemPlayer(stack=7, id="p2")
+    p3 = HoldemPlayer(stack=7, id="p3")
+    p4 = HoldemPlayer(stack=7, id="p4")
+    p5 = HoldemPlayer(stack=7, id="p5")
+    players = [p1, p2, p3, p4, p5]
+    game = HoldemGameState(players=players)
+
+    assert game.are_all_bets(0) == True
+
+def test_are_all_bets_zero_returns_false_if_one_not_zero():
+    p1 = HoldemPlayer(stack=5, id="p1")
+    p2 = HoldemPlayer(stack=7, id="p2")
+    p3 = HoldemPlayer(stack=7, id="p3")
+    p4 = HoldemPlayer(stack=7, id="p4")
+    p5 = HoldemPlayer(stack=7, id="p5")
+    p1.participate()
+    p1.bet(1)
+    players = [p1, p2, p3, p4, p5]
+    game = HoldemGameState(players=players)
+
+    assert game.are_all_bets(0) == False
+
+def test_are_all_active_bets_one_returns_true_if_all_one_and_all_active():
+    p1 = HoldemPlayer(stack=5, id="p1")
+    p2 = HoldemPlayer(stack=7, id="p2")
+    p3 = HoldemPlayer(stack=7, id="p3")
+    p4 = HoldemPlayer(stack=7, id="p4")
+    p5 = HoldemPlayer(stack=7, id="p5")
+    p1.participate()
+    p2.participate()
+    p3.participate()
+    p4.participate()
+    p5.participate()
+    p1.bet(1)
+    p2.bet(1)
+    p3.bet(1)
+    p4.bet(1)
+    p5.bet(1)
+    players = [p1, p2, p3, p4, p5]
+    game = HoldemGameState(players=players)
+
+    assert game.are_all_active_bets(1) == True
+
+def test_are_all_active_bets_one_returns_true_if_all_active_one_and_some_not_active():
+    p1 = HoldemPlayer(stack=5, id="p1")
+    p2 = HoldemPlayer(stack=7, id="p2")
+    p3 = HoldemPlayer(stack=7, id="p3")
+    p4 = HoldemPlayer(stack=7, id="p4")
+    p5 = HoldemPlayer(stack=7, id="p5")
+    p1.participate()
+    p2.participate()
+    p3.participate()
+    p4.participate()
+    p5.participate()
+    p1.bet(1)
+    p2.bet(1)
+    p3.bet(1)
+    p4.bet(1)
+    p5.bet(2)
+    p5.fold()
+    players = [p1, p2, p3, p4, p5]
+    game = HoldemGameState(players=players)
+
+    assert game.are_all_active_bets(1) == True
+
+def test_are_all_active_bets_one_returns_false_if_one_not_one_and_all_active():
+    p1 = HoldemPlayer(stack=5, id="p1")
+    p2 = HoldemPlayer(stack=7, id="p2")
+    p3 = HoldemPlayer(stack=7, id="p3")
+    p4 = HoldemPlayer(stack=7, id="p4")
+    p5 = HoldemPlayer(stack=7, id="p5")
+    p1.participate()
+    p2.participate()
+    p3.participate()
+    p4.participate()
+    p5.participate()
+    p1.bet(1)
+    p2.bet(1)
+    p3.bet(1)
+    p4.bet(1)
+    p5.bet(2)
+    players = [p1, p2, p3, p4, p5]
+    game = HoldemGameState(players=players)
+
+    assert game.are_all_active_bets(1) == False
